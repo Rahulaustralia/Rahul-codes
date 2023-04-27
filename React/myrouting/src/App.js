eb81a28757d82d151d0a6add038c0e5087f9a0a0
Rahulaustralia <rahul090897pandey@gmail.com>
2023-04-27 23:53:24 +0530

    Reacr-Router-Dom

---
+++ React/myrouting/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
