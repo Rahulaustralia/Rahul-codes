e6b1b171165fc457011c94a671c4e217afc2b96e
Rahulaustralia <rahul090897pandey@gmail.com>
2023-04-29 22:16:12 +0530

    add new

--- React/myrouting/src/App.js
+++ React/myrouting/src/App.js
@@ -1,16 +1,35 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
+import Products from "./Components/Products";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
--- React/myrouting/src/Components/Products.jsx
+++ React/myrouting/src/Components/Products.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import './Products.css'; // import the CSS file
 
 const Products = () => {
-  return (
-    <div>
-      Products
-    </div>
-  )
-}
+  
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const jsonData = await response.json();
+            setData(jsonData);
+        };
+        fetchData();
+    }, []);
 
-export default Products
+    return (
+        <div>
+            <h1>API DATA IN PRODUCT PAGE</h1>
+            <div className="product-list">
+                {data.map((item) => (
+                    <div key={item.id} className="product">
+                        <h2 className="product-title">Name:- {item.name}</h2>
+                        <p className="product-email">Email-id:- {item.email}</p>
+                        <p className="product-phone">Mobile No:- {item.phone}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Products;
