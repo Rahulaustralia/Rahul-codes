45d76374b198415cefd3e8439a864f22ba14c650
Rahulaustralia <rahul090897pandey@gmail.com>
2023-11-27 14:28:39 +0530

    add new fils

---
+++ Banglore-IN/React.JS/React_New/my-app/src/Components/Aside.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Aside = () => {
+  return (
+    <div id='side'>
+      <button style={{backgroundColor:"red",color:"white",fontSize:"20px"}}>1 </button>
+      <button style={{backgroundColor:"yellow",color:"white",fontSize:"20px"}}>2 </button>
+      <button style={{backgroundColor:"blue",color:"white",fontSize:"20px"}}>3 </button>
+      <button style={{backgroundColor:"green",color:"white",fontSize:"20px"}}>4 </button>
+    </div>
+  )
+}
+
+export default Aside
---
+++ Banglore-IN/React.JS/React_New/my-app/src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+ 
+function Navbar(){
+   let style ={
+      color:"black",
+   }
+return (
+ <div id="container">
+    <p style={style}> Logo </p>
+    <p> Home </p>
+    <p> Product </p>
+    <p> About </p>
+ </div>
+);
+
+}
+
+export default Navbar;
