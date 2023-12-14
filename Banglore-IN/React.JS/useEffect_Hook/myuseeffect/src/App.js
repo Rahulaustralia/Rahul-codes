1549f82e4bd3602f353f9b1a9abd97f19090c4c5
Rahulaustralia <rahul090897pandey@gmail.com>
2023-12-14 19:01:51 +0530

    add files

---
+++ Banglore-IN/React.JS/useEffect_Hook/myuseeffect/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "./Components/Counter";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Assignment2 from "./Components/Assignment2";
+
+function App() {
+  return (
+    <>
+      {/* <Counter /> */}
+      <Assignment2 />
+    </>
+  );
+}
+
+export default App;
---
+++ Banglore-IN/React.JS/useEffect_Hook/myuseeffect/src/Components/Counter.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+
+const Counter = () => {
+  let [count, setCount] = useState(0);
+  let [togell, setTogell] = useState(false);
+
+  useEffect(() => {
+    let time;
+    if (togell) {
+      time = setInterval(() => {
+        setCount((value) => value + 1);
+      }, 500);
+    }
+    return () => clearInterval(time);
+  }, [togell]);
+
+  function handleStart() {
+    setTogell(true);
+  }
+
+  function handleStop() {
+    setTogell(false);
+  }
+
+  function handleReset() {
+    window.location.reload();
+  }
+
+  return (
+    <div>
+      <div>
+        <div className="border border-danger w-50 p-5 m-auto mt-5 rounded">
+          <h1 style={{ textAlign: "center" }}>{count}</h1>
+          <button
+            className="btn btn-success w-25 "
+            onClick={handleStart}
+            disabled={togell}
+          >
+            start
+          </button>
+          <button
+            className="btn btn-danger w-25 m-5"
+            onClick={handleStop}
+            disabled={!togell}
+          >
+            stop
+          </button>
+          <button className="btn btn-info w-25" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
