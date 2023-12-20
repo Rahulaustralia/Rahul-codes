9ddd4bf79eba08d7d787b5c495dfba32af10d061
Rahulaustralia <rahul090897pandey@gmail.com>
2023-12-21 00:00:31 +0530

    add new updates

--- Banglore-IN/React.JS/Redux/myredux/src/Reducer/counterReducer.js
+++
@@ -1,6 +0,0 @@
-let inicialstate = {
-  inc: 1,
-  dec: 10,
-};
-
-function counterReducer(state = inicialstate, action) {}
---
+++ Banglore-IN/React.JS/Redux/myredux/src/redux/action/actionCreater.js
@@ -0,0 +1,11 @@
+import { DEC, INC, SAVE } from "./actionConstant";
+
+export function incHandler() {
+  return { type: INC };
+}
+export function decHandler() {
+  return { type: DEC };
+}
+export function saveHandler(data) {
+  return { type: SAVE, payload: data };
+}
