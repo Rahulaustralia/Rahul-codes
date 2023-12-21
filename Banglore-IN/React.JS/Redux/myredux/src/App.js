2e7da018b5e3dcf7abd85ead5d7f04dd5f8706ca
Rahulaustralia <rahul090897pandey@gmail.com>
2023-12-21 15:24:55 +0530

    add new folders

--- Banglore-IN/React.JS/Redux/myredux/src/App.js
+++ Banglore-IN/React.JS/Redux/myredux/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Card from "./Components/Card";
 import Counter1 from "./Components/Counter1";
+import Print2 from "./Components/Print2";
 
 function App() {
   return (
     <>
       <Counter1 />
+      <br />
+      <hr />
+      <Print2 />
+      <br />
+      <hr />
+      <Card />
     </>
   );
 }
