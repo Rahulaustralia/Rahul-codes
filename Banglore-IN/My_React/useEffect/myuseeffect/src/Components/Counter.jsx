7645a2e5a7ac33abb07be0b8e570420d97188fb9
Rahulaustralia <rahul090897pandey@gmail.com>
2023-12-26 01:34:39 +0530

    add new updates

--- Banglore-IN/My_React/useEffect/myuseeffect/src/Components/Counter.jsx
+++ Banglore-IN/My_React/useEffect/myuseeffect/src/Components/Counter.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1> Counter </h1>
+      <h1> Counter:- {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)} disabled={count === 0}>
+        Remove
+      </button>
     </div>
   );
 };
