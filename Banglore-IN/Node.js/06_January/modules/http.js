1ef06f2610026d3118e88c9e147b2cf36ba5ded3
Rahulaustralia <rahul090897pandey@gmail.com>
2024-01-07 01:06:15 +0530

    add new updates

--- Banglore-IN/Node.js/06_January/modules/http.js
+++ Banglore-IN/Node.js/06_January/modules/http.js
@@ -4,6 +4,7 @@
 
 const http = require("http");
 
+const fs = require("fs");
 const server = http.createServer((request, response) => {
   //   console.log(request.url);
   console.log(request.method);
@@ -14,6 +15,11 @@ const server = http.createServer((request, response) => {
     }
     if (request.url == "/about") {
       response.write("This is about page");
+      response.end();
+    }
+    if (request.url == "/contact") {
+      let data = fs.readFileSync("./msg.txt", "utf8");
+      response.write(data);
       response.end();
     }
   }
