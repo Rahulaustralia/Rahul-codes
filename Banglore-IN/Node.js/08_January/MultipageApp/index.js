f0d6cd1834f5fd2eea9d543eb751b272159ab3d4
Rahulaustralia <rahul090897pandey@gmail.com>
2024-01-10 17:34:50 +0530

    add new updates

--- Banglore-IN/Node.js/08_January/MultipageApp/index.js
+++ Banglore-IN/Node.js/08_January/MultipageApp/index.js
@@ -1,6 +1,7 @@
 // const { log } = require("console");
 const http = require("http");
 const fs = require("fs");
+const { log } = require("console");
 
 const PORT = 2000;
 const hostName = "127.0.0.4";
@@ -25,14 +26,40 @@ const server = http.createServer((req, res) => {
       res.end();
     }
     if (url == "/about") {
-      let data = fs.readFileSync("./public/html/about.html");
+      let data = fs.readFileSync("./public/html/about.html", "utf8");
       res.write(data);
       res.end();
     }
     if (url == "/contact") {
-      let data = fs.readFileSync("./public/html/contact.html");
+      let data = fs.readFileSync("./public/html/contact.html", "utf8");
       res.write(data);
       res.end();
+    }
+    if (url == "/signup") {
+      let data = fs.readFileSync("./public/html/signup.html", "utf8");
+      res.write(data);
+      res.end();
+    }
+    if (url == "/signupcss") {
+      let data = fs.readFileSync("./public/css/signup.css", "utf8");
+      res.write(data);
+      res.end();
+    }
+  }
+  if (method == "POST") {
+    if (url == "/newusers") {
+      req.on("data", (data) => {
+        console.log(data.toString());
+        let newUser = JSON.parse(data.toString());
+        let users = fs.readFileSync("./users.json", "utf-8")
+          ? JSON.parse(fs.readFileSync("./users.json", "utf-8"))
+          : [];
+        users.push(newUser);
+        console.log(newUser);
+        fs.writeFileSync("./users.json", JSON.stringify(users));
+        res.write(JSON.stringify({ msg: "Data Stored in Database" }));
+        res.end();
+      });
     }
   }
 });
