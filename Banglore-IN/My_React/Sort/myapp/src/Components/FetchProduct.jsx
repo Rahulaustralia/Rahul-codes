9209663d93e965eae472300da502ca72b1320e73
Rahulaustralia <rahul090897pandey@gmail.com>
2024-01-15 02:47:06 +0530

    add new files

---
+++ Banglore-IN/My_React/Sort/myapp/src/Components/FetchProduct.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+const FetchProduct = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products`)
+      .then((res) => res.json())
+      .then((myData) => setData(myData.products));
+    console.log(data);
+  }, []);
+
+  function handleHtoL() {
+    const sortedData = [...data].sort((a, b) => b.price - a.price);
+    setData(sortedData);
+  }
+
+  function handleLtoH() {
+    const sortedData = [...data].sort((a, b) => a.price - b.price);
+    setData(sortedData);
+  }
+
+  return (
+    <div>
+      <h1> Product Fetch </h1>
+      <button onClick={handleHtoL}> High To Low </button>
+      <button onClick={handleLtoH}> Low To High </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gap: "20px",
+        }}
+      >
+        {data.map((item) => {
+          return (
+            <>
+              <div key={item.id} style={{ border: "1px solid black" }}>
+                <img src={item.images[0]} alt="error" />
+                <h4> Brand:- {item.brand}</h4>
+                <p>Category:- {item.category}</p>
+                <h4> Price :- {item.price} </h4>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FetchProduct;
