09495ecb5965442fe333a090163f460f6e8c98ef
Rahulaustralia <rahul090897pandey@gmail.com>
2024-01-30 00:12:11 +0530

    add new files

---
+++ Banglore-IN/My_React/Formik/myapp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Practice_Formik from "./Components/Practice";
+
+function App() {
+  return (
+    <div className="App">
+      <Practice_Formik />
+    </div>
+  );
+}
+
+export default App;
---
+++ Banglore-IN/My_React/Formik/myapp/src/Components/Practice.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Practice_Formik = () => {
+  return (
+    <>
+      <form action="">
+        <label htmlFor="">UserName</label>
+        <input type="text" />
+        <br />
+        <label htmlFor="">Email</label>
+        <input type="email" />
+        <br />
+        <label htmlFor="">Password</label>
+        <input type="password" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Practice_Formik;
