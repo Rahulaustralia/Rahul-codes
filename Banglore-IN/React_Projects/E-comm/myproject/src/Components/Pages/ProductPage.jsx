5ee455b5bc349edc15f76dad952723a9e203db62
Rahulaustralia <rahul090897pandey@gmail.com>
2024-02-04 23:20:10 +0530

    add new updates

--- Banglore-IN/React_Projects/E-comm/myproject/src/Components/Pages/ProductPage.jsx
+++ Banglore-IN/React_Projects/E-comm/myproject/src/Components/Pages/ProductPage.jsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../Style/Product.css";
+import Button from "@mui/material/Button";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
 const ProductPage = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((res) => {
-      // console.log(res.data);
+      console.log(res.data);
       setData(res.data);
     });
   }, []);
 
   return (
-    <div>
-      {data.map((item) => {
-        return (
-          <>
-            <h1> {item.title}</h1>
-          </>
-        );
-      })}
+    <div className="Product-parent">
+      {data.length > 0 &&
+        data.map((item) => {
+          return (
+            <div className="product-child" key={item.id}>
+              <img src={item.image} alt="" />
+              <h4>Title:- {item.title}</h4>
+              <p>Category:- {item.category}</p>
+              <h4>Rs. {item.price}</h4>
+              <Button variant="contained" endIcon={<AddShoppingCartIcon />}>
+                {" "}
+                Add To Cart{" "}
+              </Button>
+            </div>
+          );
+        })}
+      {data.length < 0 && <h1> Loading... </h1>}
     </div>
   );
 };
