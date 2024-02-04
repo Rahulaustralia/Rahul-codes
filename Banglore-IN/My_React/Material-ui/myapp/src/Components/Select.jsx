0ce90732ba91ee9d4dcf7b3ffaa12ee80855d630
Rahulaustralia <rahul090897pandey@gmail.com>
2024-02-05 01:13:36 +0530

    add new updates

--- Banglore-IN/My_React/Material-ui/myapp/src/Components/Select.jsx
+++ Banglore-IN/My_React/Material-ui/myapp/src/Components/Select.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+
 const Select = () => {
   return (
     <div>
       <button>Normal React Button</button>
-      <Button variant="text">Text Button</Button>
-      <Button color="secondary" variant="contained">
-        Click Me
-      </Button>
-      <Button variant="outlined"> Button Outline </Button>
-      <Button href="https://google.com">Google Link</Button>
-      <Button disabled variant="contained">
-        Disabled
-      </Button>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text Button</Button>
+        <Button color="secondary" variant="contained">
+          Click Me
+        </Button>
+        <Button variant="outlined"> Button Outline </Button>
+        <Button href="https://google.com">Google Link</Button>
+        <Button disabled variant="contained">
+          Disabled
+        </Button>
+      </Stack>
+      <Box sx={{ hight: "200px", border: "2px solid blue" }}>
+        <h1> Rahul </h1>
+      </Box>
+      {/* Stack */}
     </div>
   );
 };
