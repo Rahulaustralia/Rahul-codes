4d81438b5550d8e9614087c34f49424441f4f442
Rahulaustralia <rahul090897pandey@gmail.com>
2024-09-19 22:11:21 +0530

    update code

--- 2.0/React/My_App/myapp/src/Components/Form.jsx
+++ 2.0/React/My_App/myapp/src/Components/Form.jsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const initial = {
+    userName: "",
+    password: "",
+  };
+
+  const [myForm, setMyForm] = useState(initial);
+
+  const { userName, password } = myForm;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMyForm({
+      ...myForm,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with values:", myForm);
+  };
+
   return (
     <div>
       <h1> React Form </h1>
-      <form action="">
-        <label htmlFor="">Name</label>
-        <input type="text" placeholder="Enter Your Name" id="" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Name</label>
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          value={userName}
+          name="userName"
+          onChange={handleChange}
+        />
         <br />
-        <label htmlFor="">Email</label>
-        <input type="text" placeholder="Enter your Password" id="" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          value={password}
+          name="password"
+          onChange={handleChange}
+        />
         <br />
-        <button> Submit </button>
+        <button type="submit"> Submit </button>
       </form>
+      {/* 7060370585; */}
     </div>
   );
 };
