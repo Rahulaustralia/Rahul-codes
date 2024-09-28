51fb08e43818130aa8a637c485312832bb33ad80
Rahulaustralia <rahul090897pandey@gmail.com>
2024-09-28 20:40:11 +0530

    add new update on react project

--- 2.0/React-Project/myapp/src/Components/Pages/ProductPage.jsx
+++ 2.0/React-Project/myapp/src/Components/Pages/ProductPage.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../Style/ProductPage.css";
+
 const ProductPage = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
+    axios.get("'https://dummyjson.com/products").then((res) => {
       setData(res.data);
     });
   }, []);
   console.log(data);
 
-  return <div>ProductPage</div>;
+  return (
+    <div id="RPContainer">
+      {data.map((el) => {
+        return (
+          <div key={el.id} id="RPchild">
+            <img src={el.image} alt="error" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProductPage;
